cfb8425cddb6ced58a851990cffec35b16ae6960
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-06-13 09:08:29 -0300

    Implementação da interface OnInit na classe AppComponent

--- Angular/alurapic/src/app/app.module.ts
+++ Angular/alurapic/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 
 import { PhotosModule } from './photos/photos.module';
 
@@ -13,8 +12,7 @@ import { PhotosModule } from './photos/photos.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PhotosModule,
-    HttpClientModule
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
