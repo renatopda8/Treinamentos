ab97028c9f60df1a623bf36c6dd121b396e22abb
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-06-14 09:13:03 -0300

    Implementação da interface OnChanges no PhotosComponent

--- Angular/alurapic/src/app/photos/photo-list/photos/photos.component.ts
+++ Angular/alurapic/src/app/photos/photo-list/photos/photos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Photo } from '../../photo/photo';
 
 @Component({
@@ -6,15 +6,17 @@ import { Photo } from '../../photo/photo';
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.css']
 })
-export class PhotosComponent implements OnInit {
+export class PhotosComponent implements OnChanges {
 
   @Input() photos: Photo[] = [];
   rows: any[] = [];
 
   constructor() { }
 
-  ngOnInit() {
-    this.rows = this.groupColumns(this.photos);
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.photos) {
+      this.rows = this.groupColumns(this.photos);
+    }
   }
 
   groupColumns(photos: Photo[]): any[] {
