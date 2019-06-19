dcb7467dda472c6f9fb2704ffcbf29e4047c464a
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-06-19 09:32:58 -0300

    Criação do VMessageComponente para mensagens de validação

--- Angular/alurapic/src/app/home/home.module.ts
+++ Angular/alurapic/src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { SignInComponent } from './signin/signin.component';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations: [SignInComponent],
-    imports: [CommonModule, ReactiveFormsModule]
+    imports: [CommonModule, ReactiveFormsModule, VMessageModule]
 })
 export class HomeModule { }
