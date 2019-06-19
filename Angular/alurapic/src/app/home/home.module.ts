7e0c93e8ce752930470678cb5cb12c693b00bc6f
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-06-19 09:26:27 -0300

    Validação de formulários

--- Angular/alurapic/src/app/home/home.module.ts
+++ Angular/alurapic/src/app/home/home.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 import { SignInComponent } from './signin/signin.component';
 
 @NgModule({
-    declarations: [SignInComponent]
+    declarations: [SignInComponent],
+    imports: [CommonModule, ReactiveFormsModule]
 })
 export class HomeModule { }
