227c1cca57b11d1931955cd1ca5aeb2b920709c4
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-06-25 09:33:03 -0300

    Criação do lowerCaseValidator

--- Angular/alurapic/src/app/home/signup/signup.component.ts
+++ Angular/alurapic/src/app/home/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validators';
 
 @Component({
     templateUrl: './signup.component.html'
@@ -13,7 +14,7 @@ export class SignUpComponent implements OnInit {
         this.signupForm = this.formBuilder.group({
             email: ['', [Validators.required, Validators.email]],
             fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(40)]],
-            userName: ['', [Validators.required, Validators.pattern(/^[a-z0-9_\-]+$/), Validators.minLength(2), Validators.maxLength(30)]],
+            userName: ['', [Validators.required, lowerCaseValidator, Validators.minLength(2), Validators.maxLength(30)]],
             password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(14)]],
         });
     }
