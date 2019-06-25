b0e2f415c09ee04770f2bd77bdf7bfa291571f91
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-06-25 09:53:39 -0300

    Implementação de validador para nome de usuário já utilizado

--- Angular/alurapic/src/app/home/signup/signup.component.ts
+++ Angular/alurapic/src/app/home/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validators';
+import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
 
 @Component({
     templateUrl: './signup.component.html'
@@ -8,13 +9,13 @@ import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validat
 export class SignUpComponent implements OnInit {
     signupForm: FormGroup;
 
-    constructor(private formBuilder: FormBuilder) { }
+    constructor(private formBuilder: FormBuilder, private userNotTakenValidatorService: UserNotTakenValidatorService) { }
 
     ngOnInit(): void {
         this.signupForm = this.formBuilder.group({
             email: ['', [Validators.required, Validators.email]],
             fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(40)]],
-            userName: ['', [Validators.required, lowerCaseValidator, Validators.minLength(2), Validators.maxLength(30)]],
+            userName: ['', [Validators.required, lowerCaseValidator, Validators.minLength(2), Validators.maxLength(30)], this.userNotTakenValidatorService.checkUserNameTaken()],
             password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(14)]],
         });
     }
