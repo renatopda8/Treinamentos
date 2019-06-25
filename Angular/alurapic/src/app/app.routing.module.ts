a756993f1f43b0488cb0919ee16f5f56111aa435
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-06-25 09:11:22 -0300

    Criação do SignUpComponent

--- Angular/alurapic/src/app/app.routing.module.ts
+++ Angular/alurapic/src/app/app.routing.module.ts
@@ -6,10 +6,12 @@ import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 import { SignInComponent } from './home/signin/signin.component';
+import { SignUpComponent } from './home/signup/signup.component';
 import { AuthGuard } from './core/auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: SignInComponent, canActivate: [AuthGuard] },
+  { path: 'signup', component: SignUpComponent },
   { path: 'user/:userName', component: PhotoListComponent, resolve: { photos: PhotoListResolver } },
   { path: 'photo/add', component: PhotoFormComponent },
   { path: '**', component: NotFoundComponent }
--- Angular/alurapic/src/app/home/home.module.ts
+++ Angular/alurapic/src/app/home/home.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { SignInComponent } from './signin/signin.component';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { SignUpComponent } from './signup/signup.component';
 
 @NgModule({
-    declarations: [SignInComponent],
-    imports: [CommonModule, ReactiveFormsModule, VMessageModule, RouterModule]
+    declarations: [SignInComponent, SignUpComponent],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, VMessageModule, RouterModule]
 })
 export class HomeModule { }
