11e0c27967e896cb47f6eeec8c0e7ea1ebccad1f
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-06-26 08:31:32 -0300

    Formatação de código

--- Angular/alurapic/src/app/home/signup/signup.component.ts
+++ Angular/alurapic/src/app/home/signup/signup.component.ts
@@ -12,13 +12,24 @@ import { Router } from '@angular/router';
 export class SignUpComponent implements OnInit {
     signupForm: FormGroup;
 
-    constructor(private formBuilder: FormBuilder, private userNotTakenValidatorService: UserNotTakenValidatorService, private signUpService: SignUpService, private router: Router) { }
+    constructor(
+        private formBuilder: FormBuilder,
+        private userNotTakenValidatorService: UserNotTakenValidatorService,
+        private signUpService: SignUpService,
+        private router: Router
+    ) { }
 
     ngOnInit(): void {
         this.signupForm = this.formBuilder.group({
             email: ['', [Validators.required, Validators.email]],
             fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(40)]],
-            userName: ['', [Validators.required, lowerCaseValidator, Validators.minLength(2), Validators.maxLength(30)], this.userNotTakenValidatorService.checkUserNameTaken()],
+            userName: ['', [
+                Validators.required,
+                lowerCaseValidator,
+                Validators.minLength(2),
+                Validators.maxLength(30)],
+                this.userNotTakenValidatorService.checkUserNameTaken()
+            ],
             password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(14)]],
         });
     }
