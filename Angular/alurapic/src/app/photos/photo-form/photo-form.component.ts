32b7f17100ffb63298c1ef9064d23eba2066b219
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-06-27 09:44:37 -0300

    Preview de imagens

--- Angular/alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ Angular/alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PhotoService } from '../photo/photo.service';
 import { Router } from '@angular/router';
+import { read } from 'fs';
 
 @Component({
   selector: 'ap-photo-form',
@@ -12,6 +13,7 @@ export class PhotoFormComponent implements OnInit {
 
   photoForm: FormGroup;
   file: File;
+  preview: string;
 
   constructor(private formBuilder: FormBuilder, private photoService: PhotoService, private router: Router) { }
 
@@ -27,5 +29,12 @@ export class PhotoFormComponent implements OnInit {
     const description = this.photoForm.get('description').value;
     const allowComments = this.photoForm.get('allowComments').value;
     this.photoService.upload(description, allowComments, this.file).subscribe(() => this.router.navigate(['']));
+  }
+
+  handleFile(file: File) {
+    this.file = file;
+    const reader = new FileReader();
+    reader.onload = (event: any) => this.preview = event.target.result;
+    reader.readAsDataURL(file);
   }
 }
--- Angular/alurapic/src/app/photos/photo-form/photo-form.module.ts
+++ Angular/alurapic/src/app/photos/photo-form/photo-form.module.ts
@@ -4,9 +4,10 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { PhotoFormComponent } from './photo-form.component';
 import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { PhotoModule } from '../photo/photo.module';
 
 @NgModule({
     declarations: [PhotoFormComponent],
-    imports: [CommonModule, ReactiveFormsModule, VMessageModule, FormsModule, RouterModule]
+    imports: [CommonModule, ReactiveFormsModule, VMessageModule, FormsModule, RouterModule, PhotoModule]
 })
 export class PhotoFormModule { }
