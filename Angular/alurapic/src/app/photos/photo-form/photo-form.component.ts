15acacf5f163cc3d662713be519051efe01a453d
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-06-27 09:13:59 -0300

    Obter os dados do formulário

--- Angular/alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ Angular/alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -9,6 +9,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class PhotoFormComponent implements OnInit {
 
   photoForm: FormGroup;
+  file: File;
+
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
@@ -17,5 +19,13 @@ export class PhotoFormComponent implements OnInit {
       description: ['', Validators.maxLength(300)],
       allowComments: [true]
     });
+  }
+
+  upload() {
+    const description = this.photoForm.get('description').value;
+    const allowComments = this.photoForm.get('allowComments').value;
+    console.log(description);
+    console.log(allowComments);
+    console.log(this.file);
   }
 }
