31be40bbd40d0fde18c8aa407dcf1c3ed2053080
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-06-27 09:01:07 -0300

    Inclusão do formulário de upload de fotos

--- Angular/alurapic/src/app/photos/photo-form/photo-form.module.ts
+++ Angular/alurapic/src/app/photos/photo-form/photo-form.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { PhotoFormComponent } from './photo-form.component';
+import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations: [PhotoFormComponent],
-    imports: [CommonModule]
+    imports: [CommonModule, ReactiveFormsModule, VMessageModule, FormsModule]
 })
 export class PhotoFormModule { }
