f685db2afa5a4336a06836c81b729969c5302ef0
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-06-28 08:47:06 -0300

    Criação da diretiva ImmediateClick

--- Angular/alurapic/src/app/photos/photo-form/photo-form.module.ts
+++ Angular/alurapic/src/app/photos/photo-form/photo-form.module.ts
@@ -5,9 +5,18 @@ import { PhotoFormComponent } from './photo-form.component';
 import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
 import { PhotoModule } from '../photo/photo.module';
+import { ImmediateClickModule } from 'src/app/shared/directives/immediate-click/immediate-click.module';
 
 @NgModule({
     declarations: [PhotoFormComponent],
-    imports: [CommonModule, ReactiveFormsModule, VMessageModule, FormsModule, RouterModule, PhotoModule]
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        VMessageModule,
+        FormsModule,
+        RouterModule,
+        PhotoModule,
+        ImmediateClickModule
+    ]
 })
 export class PhotoFormModule { }
