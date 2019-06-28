0222757a9e43090bd3f8020ba1c694f0bf40d718
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-06-28 08:58:09 -0300

    Bloqueando acesso não autenticado

--- Angular/alurapic/src/app/app.routing.module.ts
+++ Angular/alurapic/src/app/app.routing.module.ts
@@ -5,6 +5,7 @@ import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
+import { AuthGuard } from './core/auth/auth.guard';
 
 const routes: Routes = [
   {
@@ -17,7 +18,7 @@ const routes: Routes = [
     loadChildren: './home/home.module#HomeModule'
   },
   { path: 'user/:userName', component: PhotoListComponent, resolve: { photos: PhotoListResolver } },
-  { path: 'photo/add', component: PhotoFormComponent },
+  { path: 'photo/add', component: PhotoFormComponent, canActivate: [AuthGuard] },
   { path: '**', component: NotFoundComponent }
 ];
 
