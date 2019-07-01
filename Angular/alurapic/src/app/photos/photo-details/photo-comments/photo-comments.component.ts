2eab34d23ecd435b0fe6f1f2ec32fa930f17f98e
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-07-01 09:31:03 -0300

    Inclusão de scroll específico para a área de comentários

--- Angular/alurapic/src/app/photos/photo-details/photo-comments/photo-comments.component.ts
+++ Angular/alurapic/src/app/photos/photo-details/photo-comments/photo-comments.component.ts
@@ -8,7 +8,8 @@ import { PhotoService } from '../../photo/photo.service';
 
 @Component({
     selector: 'ap-photo-comments',
-    templateUrl: './photo-comments.component.html'
+    templateUrl: './photo-comments.component.html',
+    styleUrls: ['photo-comments.css']
 })
 export class PhotoCommentsComponent implements OnInit {
 
@@ -30,9 +31,6 @@ export class PhotoCommentsComponent implements OnInit {
         this.comments$ = this.photoService
             .addComment(this.photoId, comment)
             .pipe(switchMap(() => this.photoService.getComments(this.photoId)))
-            .pipe(tap(() => {
-                this.commentForm.reset();
-                alert('Comentário adicionado com sucesso');
-            }));
+            .pipe(tap(() => this.commentForm.reset()));
     }
 }
