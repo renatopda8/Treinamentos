f0001ceb8e44b30580e44deb5c98c4a92f5c1f00
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-07-02 09:21:52 -0300

    Melhorias no componente de alertas

--- Angular/alurapic/src/app/shared/components/alert/alert.service.ts
+++ Angular/alurapic/src/app/shared/components/alert/alert.service.ts
@@ -1,33 +1,53 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Alert, AlertType } from './alert';
+import { Router, NavigationStart } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class AlertService {
 
     alertSubject: Subject<Alert> = new Subject<Alert>();
+    keepAfterRouteChange: boolean = false;
 
-    success(message: string) {
-        this.alert(AlertType.SUCCESS, message);
+    constructor(router: Router) {
+        router.events.subscribe(event => {
+            if (event instanceof NavigationStart) {
+                if (this.keepAfterRouteChange) {
+                    this.keepAfterRouteChange = false;
+                    return;
+                }
+
+                this.clear();
+            }
+        });
     }
 
-    warning(message: string) {
-        this.alert(AlertType.WARNING, message);
+    success(message: string, keepAfterRouteChange: boolean = false) {
+        this.alert(AlertType.SUCCESS, message, keepAfterRouteChange);
     }
 
-    error(message: string) {
-        this.alert(AlertType.ERROR, message);
+    warning(message: string, keepAfterRouteChange: boolean = false) {
+        this.alert(AlertType.WARNING, message, keepAfterRouteChange);
     }
 
-    info(message: string) {
-        this.alert(AlertType.INFO, message);
+    error(message: string, keepAfterRouteChange: boolean = false) {
+        this.alert(AlertType.ERROR, message, keepAfterRouteChange);
     }
 
-    private alert(alertType: AlertType, message: string) {
+    info(message: string, keepAfterRouteChange: boolean = false) {
+        this.alert(AlertType.INFO, message, keepAfterRouteChange);
+    }
+
+    private alert(alertType: AlertType, message: string, keepAfterRouteChange: boolean) {
+        this.keepAfterRouteChange = keepAfterRouteChange;
         this.alertSubject.next(new Alert(alertType, message));
     }
 
     getAlert() {
         return this.alertSubject.asObservable();
+    }
+
+    clear() {
+        this.alertSubject.next(null);
     }
 }
