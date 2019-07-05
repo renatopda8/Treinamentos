e39052640cee5cdcec856cfe2e3522bf5e48d602
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-07-05 09:36:12 -0300

    Integração com a API de log de erros

--- Angular/alurapic/src/app/errors/global-error-handler/global-error-handler.ts
+++ Angular/alurapic/src/app/errors/global-error-handler/global-error-handler.ts
@@ -2,6 +2,7 @@ import { ErrorHandler, Injectable, Injector } from '@angular/core';
 import * as StackTrace from 'stacktrace-js';
 import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { UserService } from 'src/app/core/user/user.service';
+import { ServerLogService } from './server-log.service';
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
@@ -13,6 +14,7 @@ export class GlobalErrorHandler implements ErrorHandler {
 
         const location = this.injector.get(LocationStrategy);
         const userService = this.injector.get(UserService);
+        const serverLogService = this.injector.get(ServerLogService);
         const url = location instanceof PathLocationStrategy ? location.path() : '';
 
         const message = error.message ? error.message : error.toString();
@@ -23,8 +25,17 @@ export class GlobalErrorHandler implements ErrorHandler {
 
                 console.log(message);
                 console.log(stackAsString);
+
                 console.log('O que será enviado para o servidor');
-                console.log({message, url, userName: userService.getUserName(), stack: stackAsString});
+                serverLogService
+                    .log({ message, url, userName: userService.getUserName(), stack: stackAsString })
+                    .subscribe(
+                        () => console.log('Error logged on server.'),
+                        err => {
+                            console.log(err);
+                            console.log('Fail to send error log to server');
+                        }
+                    );
             });
     }
 }
--- Angular/alurapic/src/environments/environment.prod.ts
+++ Angular/alurapic/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://suaapientraaqui'
+  apiUrl: 'http://suaapientraaqui',
+  serverLogUrl: 'http://suaapiaqui'
 };
