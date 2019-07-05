233a64567d277de662f2b6f9f565ddfdc30b393c
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-07-05 09:00:51 -0300

    Criação do ErrorHandler

---
+++ Angular/alurapic/src/app/errors/global-error-handler/global-error-handler.ts
@@ -0,0 +1,15 @@
+import { ErrorHandler } from '@angular/core';
+import * as StackTrace from 'stacktrace-js';
+
+export class GlobalErrorHandler implements ErrorHandler {
+
+    handleError(error: any): void {
+        console.log('Passou pelo ErrorHandler');
+        StackTrace
+            .fromError(error)
+            .then(stackFrames => {
+                const stackAsString = stackFrames.map(sf => sf.toString()).join('\n');
+                console.log(stackAsString);
+            });
+    }
+}
