a5e52a4b386f5d5393ce681d11e95d9e95d520b5
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-07-08 09:15:16 -0300

    Removendo a rota da history API

--- Angular/alurapic/src/app/photos/photo-details/photo-details.component.ts
+++ Angular/alurapic/src/app/photos/photo-details/photo-details.component.ts
@@ -35,7 +35,7 @@ export class PhotoDetailsComponent implements OnInit {
     remove() {
         this.photoService.removePhoto(this.photoId).subscribe(() => {
             this.alertService.success("Photo removed", true);
-            this.router.navigate(['/user', this.userService.getUserName()]);
+            this.router.navigate(['/user', this.userService.getUserName()], { replaceUrl: true });
         }, err => {
             console.log(err);
             this.alertService.warning('Could not delete the photo!', true);
