8d04a0a678c9c8ffe78688f9db861ac5b513d441
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-07-08 08:40:27 -0300

    Redirecionamento do usuário para página de erros

--- Angular/alurapic/src/app/app.routing.module.ts
+++ Angular/alurapic/src/app/app.routing.module.ts
@@ -7,6 +7,7 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 import { AuthGuard } from './core/auth/auth.guard';
 import { PhotoDetailsComponent } from './photos/photo-details/photo-details.component';
+import { GlobalErrorComponent } from './errors/global-error/global-error.component';
 
 const routes: Routes = [
   {
@@ -45,6 +46,14 @@ const routes: Routes = [
     data:
     {
       title: 'Photo detail'
+    }
+  },
+  {
+    path: 'error',
+    component: GlobalErrorComponent,
+    data:
+    {
+      title: 'Error'
     }
   },
   {
--- Angular/alurapic/src/app/photos/photo/photo.service.ts
+++ Angular/alurapic/src/app/photos/photo/photo.service.ts
@@ -21,7 +21,7 @@ export class PhotoService {
     listFromUserPaginated(userName: string, page: number) {
         const params = new HttpParams().append('page', page.toString());
         return this.httpClient
-            .get<Photo[]>(`${API}/${userName}/photosx`, { params });
+            .get<Photo[]>(`${API}/${userName}/photos`, { params });
     }
 
     upload(description: string, allowComments: boolean, file: File) {
