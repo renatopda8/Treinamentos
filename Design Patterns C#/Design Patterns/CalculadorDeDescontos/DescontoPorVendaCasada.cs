a25cd2e990cdab24d1f9d56705a094fe3f10cd18
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-08-02 09:35:30 -0300

    Incluído o desconto por venda casada.

---
+++ Design Patterns C#/Design Patterns/CalculadorDeDescontos/DescontoPorVendaCasada.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace CalculadorDeDescontos
+{
+    public class DescontoPorVendaCasada : IDesconto
+    {
+        public IDesconto Proximo { get; set; }
+
+        public decimal Desconta(Orcamento orcamento)
+        {
+            return Existe("LAPIS", orcamento) && Existe("CANETA", orcamento) ? orcamento.Valor * 0.05m : Proximo.Desconta(orcamento);
+        }
+
+        private bool Existe(string nomeDoItem, Orcamento orcamento)
+        {
+            return orcamento.Itens.Any(it => it.Nome.Equals(nomeDoItem));
+        }
+    }
+}
