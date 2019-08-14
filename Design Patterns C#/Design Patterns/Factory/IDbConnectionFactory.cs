43b4031113328cd7ee180f1eac8c2880e1b13c4d
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-08-14 08:35:54 -0300

    Criação do projeto Factory

---
+++ Design Patterns C#/Design Patterns/Factory/IDbConnectionFactory.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Factory
+{
+    public class IDbConnectionFactory
+    {
+        public IDbConnection GetConnection()
+        {
+            IDbConnection conexao = new SqlConnection();
+            conexao.ConnectionString = "User Id=root;Password=;Server=localhost;Database=banco";
+            conexao.Open();
+            return conexao;
+        }
+    }
+}
---
+++ Design Patterns C#/Design Patterns/Factory/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+
+namespace Factory
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IDbConnection conexao = new IDbConnectionFactory().GetConnection();
+            IDbCommand comando = conexao.CreateCommand();
+            comando.CommandText = "SELECT * FROM tabela";
+        }
+    }
+}
