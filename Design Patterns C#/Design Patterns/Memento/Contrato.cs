d0271ea39b3b667a72f9e5b891b9d3978c254b5f
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-08-15 09:24:27 -0300

    Criação da classe contrato.

---
+++ Design Patterns C#/Design Patterns/Memento/Contrato.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Memento
+{
+    public class Contrato
+    {
+        public DateTime Data { get; set; }
+        public string NomeCliente { get; set; }
+        public TipoContrato Tipo { get; set; }
+
+        public void Avanca()
+        {
+            if (TipoContrato.Novo.Equals(this.Tipo))
+            {
+                this.Tipo = TipoContrato.EmAndamento;
+            }
+            else if (TipoContrato.EmAndamento.Equals(this.Tipo))
+            {
+                this.Tipo = TipoContrato.Acertado;
+            }
+            else if (TipoContrato.Acertado.Equals(this.Tipo))
+            {
+                this.Tipo = TipoContrato.Concluido;
+            }
+        }
+    }
+}
