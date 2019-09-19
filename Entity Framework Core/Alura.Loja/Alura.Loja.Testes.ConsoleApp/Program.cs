9f745cff20ec06ba77df0712fa832ba23997584a
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-09-19 10:00:33 -0300

    Alterações da aula 4

--- Entity Framework Core/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Entity Framework Core/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,56 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-
-namespace Alura.Loja.Testes.ConsoleApp
+﻿namespace Alura.Loja.Testes.ConsoleApp
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            using (var contexto = new LojaContext())
-            {
-                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
-                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
-                loggerFactory.AddProvider(SqlLoggerProvider.Create());
 
-                var produtos = contexto.Produtos.ToList();
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                var novoProduto = new Produto()
-                {
-                    Nome = "Sabão em Pó",
-                    Categoria = "Limpeza",
-                    Preco = 4.99
-                };
-                contexto.Produtos.Add(novoProduto);
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                contexto.Produtos.Remove(novoProduto);
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                //contexto.SaveChanges();
-
-                Console.WriteLine("=================");
-                var entry = contexto.Entry(novoProduto);
-                Console.WriteLine($"{entry.Entity.ToString()} - {entry.State}");
-            }
-        }
-
-        private static void ExibeEntries(IEnumerable<EntityEntry> entries)
-        {
-            Console.WriteLine("=================");
-            foreach (var e in entries)
-            {
-                Console.WriteLine($"{e.Entity.ToString()} - {e.State}");
-            }
         }
     }
 }
