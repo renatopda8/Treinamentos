965b41f5a0d3e4e119ef4f5082abf0a094d74add
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-09-23 08:59:21 -0300

    Implementação do código da aula 06

--- Entity Framework Core/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Entity Framework Core/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace Alura.Loja.Testes.ConsoleApp
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Alura.Loja.Testes.ConsoleApp
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            var paoFrances = new Produto
+            {
+                Nome = "Pão Francês",
+                PrecoUnitario = 0.40,
+                Unidade = "Unidade",
+                Categoria = "Padaria"
+            };
 
+            var compra = new Compra
+            {
+                Quantidade = 6,
+                Produto = paoFrances
+            };
+            compra.Preco = paoFrances.PrecoUnitario * compra.Quantidade;
+
+            using (var contexto = new LojaContext())
+            {
+                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+                contexto.Compras.Add(compra);
+
+                ExibeEntries(contexto.ChangeTracker.Entries());
+
+                contexto.SaveChanges();
+            }
+        }
+
+        private static void ExibeEntries(IEnumerable<EntityEntry> entries)
+        {
+            foreach (var e in entries)
+            {
+                Console.WriteLine(e.Entity + " - " + e.State);
+            }
         }
     }
 }
