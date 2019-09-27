8ddbc9d034b7247943dba34ad29883d958ebb670
Renato Albuquerque <ralbuquerque@coobrastur.com.br>
2019-09-27 08:47:56 -0300

    Implementação do código final da aula 01

--- Asp.NET Core/Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
+++ Asp.NET Core/Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -1,10 +1,21 @@
-﻿namespace Alura.ListaLeitura.App
+﻿using System.Threading.Tasks;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Alura.ListaLeitura.App
 {
     public class Startup
     {
-        public void Configure()
+        public void Configure(IApplicationBuilder app)
         {
+            app.Run(LivrosParaLer);
+        }
 
+        public Task LivrosParaLer(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.ParaLer.ToString());
         }
     }
 }
